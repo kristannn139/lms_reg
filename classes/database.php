63e6c3263f8bbd36c499dd77dbc0e4a1cee9ee73
kristannn139 <decastrok@students.nu-lipa.edu.ph>
2025-05-08 15:21:40 +0800

    lms_reg

---
+++ classes/database.php
@@ -0,0 +1,59 @@
+<?php
+class database{
+    function opencon(): PDO{
+        return new PDO(dsn: 'mysql:host=localhost;
+        dbname=lms_app.sql',
+        username: 'root',
+        password: ''
+    );
+    require_once('classes/database.php');
+    require_once('classes/functions.php');
+    $con = new database();
+    $sweetAlertConfig = "";
+ 
+    $data = $con->opencon();
+   
+    }
+ 
+    function signupUser($firstname, $lastname, $birthday, $email, $sex, $phone, $username, $password, $profile_picture_path){
+        $con = $this->opencon();
+        try {
+            $con->beginTransaction();
+            // Insert nto users table
+            $stmt = $con->prepare("INSERT INTO Users (user_Fn, user_Ln, user_birthday, user_sex, user_email, user_phone, user_username, user_password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+            $stmt->execute([$firstname, $lastname, $birthday, $sex, $email, $phone, $username, $password]);
+            // get newly inserted user_id
+            $userId = $con->lastInsertId();
+            //insert into pictures table
+            $stmt = $con->prepare("INSERT INTO users_pictures (user_id, user_pic_url) VALUES (?,?)");
+            $stmt->execute([$userId, $profile_picture_path]);
+ 
+            $con->commit();
+            return $userId;
+ 
+        } catch (PDOException $e) {
+            $con->rollBack();
+            return false;
+    }
+}
+    function insertAddress($userID, $street, $barangay, $city, $province){
+        $con = $this->opencon();
+        try {
+            $con->beginTransaction();
+            $stmt = $con->prepare("INSERT INTO Address (ba_street, ba_barangay, ba_city, ba_province)VALUES(?, ?, ?, ?)");
+            $stmt->execute([$street, $barangay, $city, $province]);
+            //get the newly inserted address_id
+            $addressId = $con->lastInsertId();
+            $stmt = $con->prepare("INSERT INTO Users_Address (user_id, address_id) VALUES (?, ?)");
+            $stmt->execute([$userID, $addressId]);
+ 
+            $con->commit();
+            return true;
+ 
+        } catch (PDOException $e) {
+            $con->rollBack();
+            return false;
+    }
+}
+}
+?>
